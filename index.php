517d2842b8710c8ca5832cf4ba8799d36d21a1ac
Justin Plein <2099449@talnet.nl>
2020-04-09 11:06:19 +0200

    opdracht gemaakt

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO</title>
+</head>
+<body>
+  <?php
+    $server = "127.0.0.1";
+    $username = "root";
+    $password ="";
+    $db = "netland123";
+
+    try {
+
+        $handle = new PDO("mysql:host=$server;dbname=$db", "$username", "$password");
+        $handle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "connected";
+    } catch (PDOException $e) {
+        die("Oops.. Something went wrong");
+    }
+  ?>
+</body>
+</html>
